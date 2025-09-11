55a2ac2b28c7d35f98bb627433a9e4b409604daa
Finch <Finchasaurus@gmail.com>
2025-09-10 22:06:49 -0600

    yes

--- posts.js
+++ posts.js
@@ -6,13 +6,29 @@ fetch(postsLink)
 	.then((data) => {
 		if (!data.posts) return;
 		data.posts.forEach((post) => {
-			const postEl = document.createElement("div");
+			const id = post.id;
+			const title = post.title;
+			const description = post.description;
+			const date = post.date || "Unknown date";
+			const tags = post.tags || [];
+			const image = post.image;
+
+			const postEl = document.createElement("a");
 			postEl.className = "post";
+			postEl.href = `/posts/${post.id}`;
+			postEl.title = `${post.title} - ${post.description}`;
 			postEl.innerHTML = `
-        <h3>${post.title}</h3>
-        <p>${post.description}</p>
-        <a href="/posts/${post.id}">Read more</a>
-      `;
+                <h3>${post.title}</h3>
+                <p>${post.description}</p>
+                <p><small>${post.date} | Tags: ${post.tags.join(", ")}</small></p>
+            `;
+			if (image) {
+				const imgEl = document.createElement("img");
+				imgEl.src = image;
+				imgEl.alt = title;
+				imgEl.style.maxWidth = "100%";
+				postEl.prepend(imgEl);
+			}
 			postsContainer.appendChild(postEl);
 		});
 	})
